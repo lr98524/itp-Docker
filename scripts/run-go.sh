c9142f00da7d5da8a04e8b402f0a13f3cf292c31
Liam <lr98524@email.vccs.edu>
2025-02-19 15:44:43 -0500

    Modifying the shell script to compile a go proram with one container and run it with another

--- scripts/run-go.sh
+++ scripts/run-go.sh
@@ -1,7 +1,12 @@
-#MSYS_NO_PATHCONV=1 
+#MSYS_NO_PATHCONV=1
 
 #pull the base image
 docker pull golang:tip-alpine3.21
 
-#run the sh in the container 
-MSYS_NO_PATHCONV=1 docker run --rm -it --name go-c --workdir /go/src -v $(pwd)/src:/go/src golang:tip-alpine3.21 sh
+#compiles a go program
+MSYS_NO_PATHCONV=1 docker run --rm --name go-c --workdir /go/src -v $(pwd)/src:/go/src golang:tip-alpine3.21 go build program.go
+
+#run the compiled program by pulling an alpine image
+docker pull alpine:3.21.3
+#running the compiled program
+ MSYS_NO_PATHCONV=1 docker run --rm --name runner-c -v  $(pwd)/src/program:/bin/program alpine:3.21.3 program
