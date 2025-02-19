95e0cdf9405abe5a1d73a9584f0e6ab9d421fd2e
Liam <lr98524@email.vccs.edu>
2025-02-19 14:48:20 -0500

    Made it so it ran hade volume creation, also can now see programs in container

--- scripts/run-go.sh
+++ scripts/run-go.sh
@@ -4,4 +4,4 @@
 docker pull golang:tip-alpine3.21
 
 #run the sh in the container 
-MSYS_NO_PATHCONV=1 docker run --rm -it --name go-c --workdir /go/src golang:tip-alpine3.21
+MSYS_NO_PATHCONV=1 docker run --rm -it --name go-c --workdir /go/src -v $(pwd)/src:/go/src golang:tip-alpine3.21 sh
