5c8a3b77f5afd2fbf58ca610c939a7f06411905c
Liam <lr98524@email.vccs.edu>
2025-02-19 14:17:33 -0500

    Script: running sh inside golang container

--- run-go.sh
+++ run-go.sh
@@ -1,2 +1,7 @@
+#MSYS_NO_PATHCONV=1 
 
+#pull the base image
 docker pull golang:tip-alpine3.21
+
+#run the sh in the container 
+MSYS_NO_PATHCONV=1 docker run --rm -it --name go-c --workdir /go/src golang:tip-alpine3.21
